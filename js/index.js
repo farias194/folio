d710206565a851f4efa8970b44f3e9c2693f2989
farias194 <ariasfrank057@gmail.com>
2024-11-30 11:14:16 -0500

    s

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 const nav= document.getElementById('nav');
  const navC = nav.children;
 window.addEventListener('scroll', scrollnav);
@@ -23,7 +24,45 @@ const overlayName = document.querySelectorAll('.projectName a')
 console.log(overlayName)
 
 
+const project = document.querySelectorAll('.project');
 
+const changeBackGroundOnScroll =() =>{
+    project.forEach((project) => {
+        const projectBorder = project.getBoundingClientRect();
+        if (projectBorder.top <= window.innerHeight / 2 && projectBorder.bottom >= window.innerHeight / 2 ){
+            document.getElementById('MasterProjectWrapper').style.background = project.getAttribute('data-bg-gradient');
+        }
+    });
+};
+
+window.addEventListener('scroll', changeBackGroundOnScroll);
+console.log(document.getElementById('MasterProjectWrapper'));
+
+
+const texts = document.querySelectorAll('.carouselText');
+let currentIndex = 0;
+
+
+const showText = (index)=> {
+    texts.forEach((text, i ) =>{
+        if (i == index ){
+            text.style.opacity = 1;
+
+        }
+        else{
+            text.style.opacity= 0;
+        }
+    })
+}
+
+const startCarousel = () =>{
+    showText (currentIndex);
+    setInterval(() =>{
+        currentIndex= (currentIndex + 1) % texts.length;
+        showText(currentIndex)
+    }, 3000)
+}
+startCarousel();
 /*function bgChange(){
 
     const y = window.scrollY;
@@ -33,4 +72,4 @@ document.getElementById('MasterProjectWrapper').style.backgroundColor='black'
 else
 document.getElementById('MasterProjectWrapper').style.backgroundColor='white'
 }
-window.addEventListener('scroll', bgChange)/*
+window.addEventListener('scroll', bgChange)*/
