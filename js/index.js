2c037ac495abd209979c017f3f2f2e770cc2bebf
farias194 <ariasfrank057@gmail.com>
2024-12-10 14:28:00 -0500

    s

--- js/index.js
+++ js/index.js
@@ -75,3 +75,12 @@ else
 document.getElementById('MasterProjectWrapper').style.backgroundColor='white'
 }
 window.addEventListener('scroll', bgChange)*/
+
+/*
+What Javascript statement in place of "?" will make the result always be between 6 and 7?*/
+
+const x = 2; let y = 4; 
+function update(arg) { return Math.random() + y * arg; } y = 2; 
+y++;
+ const result = update(x);
+ console.log(result) 
